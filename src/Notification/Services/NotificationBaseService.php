592562f21be0521f3913f4ebe56c8354c2cc476f
mattybaer <mbaer@bibliopolis.com>
2021-08-10 20:27:16 -0400

    Create NotificationBaseService.php

---
+++ src/Notification/Services/NotificationBaseService.php
@@ -0,0 +1,66 @@
+<?php
+namespace DTS\eBaySDK\Notification\Services;
+
+/**
+ * Base class for the Compliance service.
+ */
+class NotificationBaseService extends \DTS\eBaySDK\Services\BaseRestService
+{
+    /**
+     * @var array $endPoints The API endpoints.
+     */
+    protected static $endPoints = [
+        'sandbox'    => 'https://api.sandbox.ebay.com/commerce/notification',
+        'production' => 'https://api.ebay.com/commerce/notification'
+    ];
+
+    /**
+     * HTTP header constant. The Authentication Token that is used to validate the caller has permission to access the eBay servers.
+     */
+    const HDR_AUTHORIZATION = 'Authorization';
+
+    /**
+     * @param array $config Configuration option values.
+     */
+    public function __construct(array $config)
+    {
+        parent::__construct($config);
+    }
+
+    /**
+     * Returns definitions for each configuration option that is supported.
+     *
+     * @return array An associative array of configuration definitions.
+     */
+    public static function getConfigDefinitions()
+    {
+        $definitions = parent::getConfigDefinitions();
+
+        return $definitions + [
+            'apiVersion' => [
+                'valid' => ['string'],
+                'default' => \DTS\eBaySDK\Compliance\Services\NotificationService::API_VERSION,
+                'required' => true
+            ],
+            'authorization' => [
+                'valid' => ['string'],
+                'required' => true
+            ]
+        ];
+    }
+
+    /**
+     * Builds the needed eBay HTTP headers.
+     *
+     * @return array An associative array of eBay HTTP headers.
+     */
+    protected function getEbayHeaders()
+    {
+        $headers = [];
+
+        // Add required headers first.
+        $headers[self::HDR_AUTHORIZATION] = 'Bearer '.$this->getConfig('authorization');
+
+        return $headers;
+    }
+}
